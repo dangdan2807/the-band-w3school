270ff9a7f1a7bd1e835871a241759843bf60bd16
KayJuno <dangdan2807@gmail.com>
2021-07-03 18:27:49 +0700

    Mobile menu responsive

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Demo các trang web
 
 ## Demo một phần của Shopee  
-- Được viết bằng html, css và FontAwesome icon thuần  
+- Được viết bằng html, css thuần  
 - FontAwesome version: 5.15.1  
     ![demo shop](./img/demo-shop.png)
 
 ## Demo W3.CSS Templates: Band Template
-- Được viết bằng html, css và FontAwesome icon thuần 
-- themify icons
+- Được viết bằng html, css và javascript thuần 
+- FontAwesome version: 5.15.1
 ![The band template](https://i.imgur.com/8nJ80pm.jpg)
--- the-band-w3school/assets/js/index.js
+++ the-band-w3school/assets/js/index.js
@@ -1,33 +1,56 @@
+// change image
 var sliderInfo = [
     {
         urlImg: "/assets/img/slider1.jpg",
-        heading: 'Los Angeles',
-        description: 'We had the best time playing at Venice Beach!'
+        heading: "Los Angeles",
+        description: "We had the best time playing at Venice Beach!",
     },
     {
         urlImg: "/assets/img/slider2.jpg",
-        heading: 'New York',
-        description: 'The atmosphere in New York is lorem ipsum.'
+        heading: "New York",
+        description: "The atmosphere in New York is lorem ipsum.",
     },
     {
         urlImg: "/assets/img/slider3.jpg",
-        heading: 'Chicago',
-        description: "Thank you, Chicago - A night we won't forget."
-    }
+        heading: "Chicago",
+        description: "Thank you, Chicago - A night we won't forget.",
+    },
 ];
 
 var index = 0;
 
 function changeImages() {
-    if (index > 2)
-        index = 0;
-    var sliderElement = document.getElementsByClassName('slider')[0];
-    var sliderHeading = document.getElementsByClassName('text-heading')[0];
-    var sliderDescription = document.getElementsByClassName('text-description')[0];
-    sliderElement.style.backgroundImage = 'url(' + sliderInfo[index].urlImg + ')';
+    if (index > 2) index = 0;
+    var sliderElement = document.getElementsByClassName("slider")[0];
+    var sliderHeading = document.getElementsByClassName("text-heading")[0];
+    var sliderDescription =
+        document.getElementsByClassName("text-description")[0];
+    sliderElement.style.backgroundImage =
+        "url(" + sliderInfo[index].urlImg + ")";
     sliderHeading.innerHTML = sliderInfo[index].heading;
     sliderDescription.innerHTML = sliderInfo[index].description;
     index++;
 }
-
 setInterval(changeImages, 3000);
+
+// event onclick mobile menu btn
+var headerElement = document.getElementsByClassName("header")[0];
+var mobileMenu = document.getElementsByClassName('mobile-menu-btn')[0];
+var headerHeight = headerElement.clientHeight;
+
+mobileMenu.onclick = function() {
+    var isClose = headerElement.clientHeight === headerHeight;
+    if (isClose)
+        headerElement.style.height = "auto";
+    else
+        headerElement.style.height = headerHeight + "px";
+};
+
+// event onclick menu item
+var menuItems = document.querySelectorAll('.nav a[href*="#"].nav__link');
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    menuItem.onclick = function () {
+        headerElement.style.height = headerHeight + "px";
+    }
+}
